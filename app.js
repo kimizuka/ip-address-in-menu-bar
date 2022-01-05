1965ff9bc90261f099af73406bb46579a040c5f6
kimizuka <f.kimizuka@gmail.com>
2022-01-05 11:29:48 +0900

    :)

---
+++ app.js
@@ -0,0 +1,29 @@
+const { app, Tray, Menu, BrowserWindow } = require('electron');
+const ip = require('ip');
+const path = `${ __dirname }/icon.png`;
+
+let tray;
+
+app.on('ready', () => {
+  const menu = new Menu.buildFromTemplate([{
+    label: 'reload',
+    click: render
+  },{
+    label: 'exit',
+    role: 'quit'
+  }]);
+
+  app.dock.hide();
+
+  tray = new Tray(path);
+  tray.setContextMenu(menu);
+
+  render();
+});
+
+function render() {
+  const address = ip.address();
+
+  tray.setToolTip(address);
+  tray.setTitle(address);
+}
